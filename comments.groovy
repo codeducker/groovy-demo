84f7e5843304e4cf195f020dcd7da592d0ff64fc
codeducker <fyhzyt@gmail.com>
2024-01-16 14:12:45 +0800

    Create comments.groovy
    提交注释代码

---
+++ comments.groovy
@@ -0,0 +1,34 @@
+println "hello" //单行注释
+
+
+/**
+  多行注释
+*/
+println "mutiple comments" 
+
+/**
+  文档注释
+  可以使用JavaDoc相关 
+ @param name 名称
+  @return 格式化欢迎
+*/
+String greet(String name){
+  return "你好 ${name}"
+}
+
+
+/**@
+  *另外 3.xx版本支持 运行时groovy文档,默认是关闭的 需要通过 -Dgroovy.attach.runtime.groovydoc=true
+  *Some class groovydoc 运行时groovydoc
+*/
+class Foo {
+
+  /**@
+  * some method groovy for bar
+  */
+  void bar(){
+
+  }
+}
+assert Foo.class.groovydoc.content.contains('另外')
+assert Foo.class.getMethod('bar',new Class[0]).groovydoc.content.contains('method')
