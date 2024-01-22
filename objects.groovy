7ce6c95c950eb19f7db08ede417651148cf9f22c
codeducker <fyhzyt@gmail.com>
2024-01-22 15:48:31 +0800

    新增面向对象知识点

--- objects.groovy
+++ objects.groovy
@@ -42,291 +42,247 @@ new SomeClass().talk()
 def sqlDate = new SqlDate(1000)
 println sqlDate
 
-////        def time = t("")
-////        add(1,1)
-////        def t = from("2023-06-05 00:00:00")
-////        add(1l,2l)
-//        assert now().class == Calendar.getInstance().class
-//        d(9,Random.newInstance())
-////        add(1l,2l)
-////        sin()
-//        parseLong("1")
-//        t(11,new Random())
-//        SqlDate date = new SqlDate(1000)
-   // }
-// }
+class Foo2 {
+  static int i 
+}
 
-//class Foo {
-//    static int i
-//}
-//
-//assert Foo.class.getDeclaredField('i').type == int.class //原始类型
-//assert Foo.i.class != int.class && Foo.i.class == Integer.class //包装类型
-//
-//
-//class Outer {
-//    private String privateStr
-//
-//    def callInnerMethod() {
-//        new Inner().methodA()
-//    }
-//
-//    class Inner {
-//        def methodA() {
-//            println "${privateStr}."
-//        }
-//    }
-//}
+assert Foo2.class.getDeclaredField('i').type == int.class
+assert Foo2.i.class != int.class && Foo2.i.class == Integer.class
 
-//def inner = new Outer().new Inner()
+class Outer{
 
-//interface Fao {
-//    void check()
-//}
-//
-//interface Gao {
-//
-//}
-//
-//abstract class A implements Fao, Gao {
-//
-//}
-//
-//class LastGao {
-//    void check() {
-//        println("check")
-//    }
-//}
-
-//def gao = new LastGao()
-//def foo = gao as Fao
-//assert gao instanceof  Fao
-
-//def a = new BigInteger(1) as Long //运行时
-//assert a instanceof Long
-//
-//class PersonConstructor {
-//    String name
-//    Integer age
-//
-//    PersonConstructor(name, age) {
-//        this.name = name
-//        this.age = age
-//    }
-//}
-//
-//def person1 = new PersonConstructor('Marie', 1)
-//def person2 = ['Marie', 2] as PersonConstructor
-//PersonConstructor person3 = ['Marie', 3]
-//
-//
-//class See {
-//
-//}
-//
-//def see = new See()
-//def see2 = [] as See
-//See see3 = []
-//
-//class Joke {
-//    String name
-//    int id
-//
-//    Joke(int id, String name) {
-//        this.id = id
-//        this.name = name
-//    }
-//}
-//
-//String name = "andy"
-//int id = 3
-//
-//def joke = new Joke(id, name)
-//def joke1 = [id, name] as Joke
-//Joke joke2 = [id, name]
-//
-//class User {
-//    Long id
-//    String name
-//}
-//
-//def user = new User(id: 1, name: 'Alex')
-//
-//class Take {
-//    Map lock
-//    String name
-//}
-//
-//def take = new Take(lock: ["id": 1])
-//
-//def someMethod() { 'method called' }
-//
-//def method = someMethod()
-//println(method)
-//
-//def foo(Map args) { "${args.name}: ${args.age}" }
-//
-//foo(name: 'Marie', age: 1)
-//
-//def fooLook(int id, String name) {
-//    "${id}----${name}"
-//}
-//
-//fooLook(1, "hello")
-//
-//def foo(Map args, int id) { println "${id} : ${args.name}: ${args.age} " }
-//
-//foo(name: 'Marie', age: 1, 1)
-//foo(name: 'Marie', 1, age: 1)
-//foo(1, name: 'Marie', age: 12)
-//
-//def foo(String id, Map args) { println "second=> ${id} : ${args.name}: ${args.age} " }
-//
-//def map = [name: 'Marie', age: 11]
-//foo("1", map)
-//foo( name: 'Marie', age: 1, 1)
-
-//def defaultValue(int id = 0, String name, String address) {
-//    println "${id} : ${name} : ${address}"
-//}
-//
-//defaultValue( "hello", "lock")
-//
-//
-//def foo2(String par1, Integer par2 = 1) { [name: par1, age: par2] }
-//
-//assert foo2('Marie').age == 1
-//
-//def baz(a = 'a', int b, c = 'c', boolean d, e = 'e') { "$a $b $c $d $e" }
-//
-//assert baz(42, true) == 'a 42 c true e'
-//assert baz('A', 42, true) == 'A 42 c true e'
-//assert baz('A', 42, 'C', true) == 'A 42 C true e'
-//assert baz('A', 42, 'C', true, 'E') == 'A 42 C true E'
-//
-//
-//def bas(ax = 'a', int b, c = 'c', boolean d, e = 'e', boolean f = false) {
-//    println "$ax $b $c $d $e $f"
-//}
-//
-//bas(42, 1,  false, true,1,true)
-
-//assert  == 'a 42 c true e'
-//assert bas('A', 42, true) == 'A 42 c true e'
-//assert bas('A', 42, 'C', true) == 'A 42 C true e'
-//assert bas('A', 42, 'C', true, 'E') == 'A 42 C true E'
-
-//def foo(Object... args) { 1 }
-//def foo(Object x) { 2 }
-//assert foo() == 1
-//assert foo(1) == 2
-//assert foo(1, 2) == 1
-
-//def method(Object o1, Object o2) { 'o/o' }
-//def method(Integer i, String  s) { 'i/s' }
-//def method(String  s, Integer i) { 's/i' }
-//
-//List<List<Object>> pairs = [['foo', 1], [2, 'bar'], [3, 4]]
-//assert pairs.collect { a, b -> method(a, b) } == ['s/i', 'i/s', 'o/o']
-//
-//pairs = [['foo', 1], [2, 'bar'], [3, "true"]]
-//pairs.collect { a, b -> println "${a} ${b}"}
-//assert pairs.collect { a, b -> method(a, b) } == ['s/i', 'i/s', 'i/s']
-
-//interface A {
-//
-//}
-//class B {
-//
-//}
-//class C extends B implements A {
-//
-//}
-//def method(A a){
-//    println("interface")
-//}
-//
-//def method(B b){
-//    println("Super Class")
-//}
-//
-//method(new C())
-//
-//def method(Date d, Object o) { 'd/o' }
-//def method(Object o, String s) { 'o/s' }
-//
-//println method(new Date(), 'baz')
-
-//assert method(new Date(), (Object)'baz') == 'd/o'
-//assert method((Object)new Date(), 'baz') == 'o/s'
-
-//class Person {
-//    String name
-//    void name(String name) {
-//        this.name = "Wonder $name"
-//    }
-//    String title() {
-//        this.name
-//    }
-//}
-//def p = new Person()
-//p.name = 'Diana'
-//assert p.name == 'Diana'
-//
-//
-//
-////p.name('Woman')
-//p.name = 'Wonder Woman'
-//assert p.title() == 'Wonder Woman'
-//
-//for (final def ps in p.properties.keySet() ) {
-//    println(ps)
-//}
-
-//class PseudoProperties {
-//     String name
-//
-//    String joo
-//    String getJoo() {
-//        return joo
-//    }
-//
-//    void setJoo(String joo) {
-//        this.joo = joo
-//    }
-//
-//    String getFoo(){ "foo"}
-//
-//    void setFoo(String foo){this.foo = foo}
-//
-//    // a pseudo property "name"
-//    void setName(String name) {this.name =name}
-//    String getName() { "lucky"}
-//
-//    // a pseudo read-only property "age"
-//    int getAge() { 42 }
-//
-//    // a pseudo write-only property "groovy"
-//    void setGroovy(boolean groovy) {  }
-//}
-//def p = new PseudoProperties()
-//p.name = 'Foo'
-//println(p.@name)
-//println(p.name)//lucky 使用getName获取
-//assert p.age == 42
-//p.groovy = true
-//println(p.foo)
-//println(p.Foo)
+  private String privateStr
 
 
-//class Animal {
-//    int lowerCount = 0
-//    @Lazy String name = { lower().toUpperCase() }()
-//    String lower() { lowerCount++; 'sloth' }
-//}
-//
-//def a = new Animal()
-//assert a.lowerCount == 0
-//assert a.name == 'SLOTH'
-//assert a.lowerCount == 1
+  def callInnerMethod(){
+    new Inner().methodA()
+  }
+
+  def callAsyncInnerMethod(){
+    new Thread(new Inner2()).start()
+  }
+
+  class Inner{
+    def methodA(){
+      println "${privateStr}"
+    }
+  }
+
+  class Inner2 implements Runnable{
+    void run(){
+      println "${privateStr}"
+    }
+  }
+}
+
+new Outer(privateStr:"hello").callInnerMethod()
+new Outer(privateStr: "neck").callAsyncInnerMethod()
+
+class Computer{
+  class Cpu{
+    int coreNumber
+
+    Cpu(int coreNumber){
+      this.coreNumber = coreNumber
+    }
+  }
+}
+
+assert 4 == new Computer().new Cpu(4).coreNumber
+
+interface Greeter{
+  void greet(String name);
+
+  default void say(){
+    println "say"
+  }
+}
+
+class SystemGreater implements Greeter {
+  void greet(String name){
+    println "Hello $name"
+  }
+}
+
+def greeter = new SystemGreater()
+greeter.greet("like ")
+assert greeter instanceof Greeter
+
+interface ExtendedGreeter extends Greeter {
+  void sayGoods()
+}
+
+class DefaultGreeter {
+  void greet(String name) { println "defaut $name"}
+}
+def defaultGreeter = new DefaultGreeter()
+assert !(defaultGreeter instanceof Greeter)
+
+
+class PersonConstructor{
+  String name
+  Integer age 
+  PersonConstructor (name,age){
+    this.name = name
+    this.age = age 
+  }
+}
+
+def person1 = new PersonConstructor('Marie',1)
+def person2 = ['Marie',2] as PersonConstructor
+PersonConstructor person3 = ['Marie',3]
+
+class PersonWoConstructor{
+  String name
+  Integer age 
+
+  def someMethod(){"method called"}
+  String anotherMethod() {"another method called"}
+  def thirdMethod(params) {"$params exxcuted"}
+  static String fourthMethod(params) {"static $params called "}
+}
+def person4 = new PersonWoConstructor()
+def person5 = new PersonWoConstructor(name:"Marie")
+def person6 = new PersonWoConstructor(age:1)
+
+def fooMap(Map args){
+  "${args.age}, ${args.name}"
+}
+println fooMap(name:"Marie", age:1)
+println fooMap(name:"puot")
+def fooMixMap(Map map ,Integer num){
+  "$map.age and $map.name is the number $num"
+}
+
+println fooMixMap(name: "more" , age:2,23)
+println fooMixMap(12,name:"yuki",age:4)
+
+def fooSome(Integer number,Map args){
+  "$args.name not $args.age is $number"
+}
+
+// 如果存在map函数,那么此函数map参数必须要在第一位,否则名称参数方式调用会抛出异常
+
+// fooSome(name:'just',age:13,23)
+fooSome(23,[name:'foo',age:10])
+
+def defaultValue(int id = 0, String name, String address) {
+   println "${id} : ${name} : ${address}"
+}
+
+defaultValue( "hello", "lock")
+
+
+def foo2(String par1, Integer par2 = 1) { [name: par1, age: par2] }
+assert foo2('Marie').age == 1
+
+def baz(a = 'a', int b, c = 'c', boolean d, e = 'e') { "$a $b $c $d $e" }
+
+assert baz(42, true) == 'a 42 c true e'
+assert baz('A', 42, true) == 'A 42 c true e'
+assert baz('A', 42, 'C', true) == 'A 42 C true e'
+assert baz('A', 42, 'C', true, 'E') == 'A 42 C true E'
+
+
+def fooMethod(Object... args) { args.length }
+def fooMethod(Object x) { 2 }
+assert fooMethod() == 0
+assert fooMethod(1) == 2
+assert fooMethod(1, 2) == 2
+
+def fooArgs(Object[] args) {args.length}
+assert fooArgs() == 0
+assert fooArgs(1) == 1 
+assert fooArgs(2,3) == 2
+// assert fooArgs(null) == null //这边会空指针异常
+
+def method(Object o1, Object o2) { 'o/o' }
+def method(Integer i, String  s) { 'i/s' }
+def method(String  s, Integer i) { 's/i' }
+
+assert method('foo' ,42) == 's/i'
+
+List<List<Object>> pairs = [['foo', 1], [2, 'bar'], [3, 4]]
+assert pairs.collect { a, b -> method(a, b) } == ['s/i', 'i/s', 'o/o']
+
+interface A {}
+class B {}
+class C extends B implements A {}
+def method(A a){ 
+   println("interface")
+}
+
+def method(B b){
+   println("Super Class")
+}
+
+method(new C())
+
+def methodC(Date d, Object o) { 'd/o' }
+def methodC(Object o, String s) { 'o/s' }
+
+
+import static groovy.test.GroovyAssert.shouldFail
+def ex = shouldFail {
+  println methodC(new Date(), 'baz')
+}
+assert ex.message.contains('Ambiguous method overloading')
+
+
+class Data{
+  private int id 
+  protected String description
+  public static final boolean DEBUG = false 
+  final String name
+  Data(String name){
+    this.name = name
+  }
+}
+
+class Person {
+   String name
+   void name(String name) {
+       this.name = "Wonder $name"
+   }
+   String title() {
+       this.name
+   }
+}
+def p = new Person()
+p.name = 'Diana'
+assert p.name == 'Diana'
+
+p.name('Woman')
+p.name = 'Wonder Woman'
+assert p.title() == 'Wonder Woman'
+
+for (final def ps in p.properties.keySet() ) {
+   println(ps)
+}
+
+class PseudoProperties {
+   // a pseudo property "name"
+   void setName(String name) {}
+   String getName() { "lucky"}
+}
+
+def pi = new PseudoProperties()
+pi.name = 'Foo'
+// println(pi.@name) //这里因为未定义name属性,所以会报异常
+println(pi.name)//lucky 使用getName获取
+
+import groovy.transform.*
+
+class Animal {
+   int lowerCount = 0
+   @Lazy String name = { lower().toUpperCase() }()
+   @PackageScope String address
+   @Synchronized int getAge(){} 
+   String lower() { lowerCount++; 'sloth' }
+}
+
+def a = new Animal()
+assert a.lowerCount == 0
+assert a.name == 'SLOTH'
+assert a.lowerCount == 1
 
